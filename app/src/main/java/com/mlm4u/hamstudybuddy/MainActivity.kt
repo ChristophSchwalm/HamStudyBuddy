50dfbf4083d46a31dba9b429f1063204514cf2a6
Christoph Schwalm <chris@christoph-schwalm.de>
2024-08-28 21:53:10 +0200

    HamStudyBuddy V0.003 Navigation Check

--- app/src/main/java/com/mlm4u/hamstudybuddy/MainActivity.kt
+++ app/src/main/java/com/mlm4u/hamstudybuddy/MainActivity.kt
@@ -2,10 +2,47 @@ package com.mlm4u.hamstudybuddy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
+import androidx.navigation.ui.setupWithNavController
+import com.mlm4u.hamstudybuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vb: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
+        vb.bottomNavigationView.setupWithNavController(navController)
+
+        vb.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bootcampFragment -> {
+                    val navOptions = NavOptions.Builder()
+                        .setPopUpTo(R.id.bootcampFragment, true)
+                        .build()
+                    navController.navigate(R.id.bootcampFragment, null, navOptions)
+                    true
+                }
+
+                else -> {
+                    navController.navigate(item.itemId)
+                true
+                }
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/mlm4u/hamstudybuddy/ui/PoolFragment.kt
+++ app/src/main/java/com/mlm4u/hamstudybuddy/ui/PoolFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.mlm4u.hamstudybuddy.R
 
-class QuestionPoolFragment : Fragment() {
+class PoolFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_pool, container, false)
+        return inflater.inflate(R.layout.fragment_pool, container, false)
     }
 
 }
