a1d03f4719afc98c6c67e82b128b9a78a4b81226
ChristophSchwalm <chris@christoph-schwalm.de>
2024-09-01 00:17:32 +0200

    HamStudyBuddy V0.013 min. Anforderungen erfüllt

--- app/src/main/java/com/mlm4u/hamstudybuddy/MainActivity.kt
+++ app/src/main/java/com/mlm4u/hamstudybuddy/MainActivity.kt
@@ -53,48 +53,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         Log.d("MainActivity", "${sharedViewModel.currentUser}")
-        api()
+
     }
-
-        private fun api () {
-            Log.d("MainActivity", "onCreate called")
-            lifecycleScope.launch {
-                Log.d("MainActivity", "lifecycleScope.launch called")
-                try {
-                    val questionRemoteApi = QuestionApi.retrofitService
-                    Log.d("MainActivity", "QuestionRemoteApi created")
-                    val data: Root = questionRemoteApi.getQuestions()
-
-                    Log.d("MainActivity", "Data: $data")
-                    data.let {
-                        // Alle Questions aus den JSON-Daten extrahieren
-
-                        val allQuestions = it.getAllQuestions()
-                        Log.d("MainActivity", "Anzahl der Fragen: ${allQuestions.size}")
-                        // Die Questions in UserQuestions umwandeln
-                        val gameQuestions: List<Questions> = allQuestions.map { question ->
-                            question.toUserQuestion()
-                        }
-
-                        // Die Fragen in die Datenbank einfügen
-                        Log.d("MainActivity", "Vor Insert:")
-                        sharedViewModel.insertQuestions(gameQuestions)
-                        Log.d("MainActivity", "Nach Insert:")
-                    }
-                } catch (e: Exception) {
-                    // Fehlerbehandlung, z. B. Anzeige einer Fehlermeldung
-                    Log.e("MainActivity", "Fehler Api -> Room: ${e.message}")
-                }
-            }
-
-            lifecycleScope.launch {
-                try {
-                    val version = QuestionApi.retrofitService.getVersion()
-                    Log.d("MainActivity", "Version: ${version.version}")
-                } catch (e: Exception) {
-                    Log.e("MainActivity", "VersiongCheck Error: ${e.message}")
-                }
-            }
-        }
 
 }
--- app/src/main/java/com/mlm4u/hamstudybuddy/ui/SettingsFragment.kt
+++ app/src/main/java/com/mlm4u/hamstudybuddy/ui/SettingsFragment.kt
@@ -1,12 +1,16 @@
 package com.mlm4u.hamstudybuddy.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import com.mlm4u.hamstudybuddy.data.database.Questions
+import com.mlm4u.hamstudybuddy.data.model.Root
+import com.mlm4u.hamstudybuddy.data.remote.QuestionApi
 import com.mlm4u.hamstudybuddy.data.viewModel.SharedViewModel
 import com.mlm4u.hamstudybuddy.databinding.FragmentSettingsBinding
 import kotlinx.coroutines.launch
@@ -58,5 +62,50 @@ class SettingsFragment : Fragment() {
             sharedViewModel.saveUserSettings(vb.teName.text.toString())
         }
 
+        vb.btUpdate.setOnClickListener{
+            api()
+        }
+
+    }
+
+    private fun api () {
+        Log.d("MainActivity", "onCreate called")
+        lifecycleScope.launch {
+            Log.d("MainActivity", "lifecycleScope.launch called")
+            try {
+                val questionRemoteApi = QuestionApi.retrofitService
+                Log.d("MainActivity", "QuestionRemoteApi created")
+                val data: Root = questionRemoteApi.getQuestions()
+
+                Log.d("MainActivity", "Data: $data")
+                data.let {
+                    // Alle Questions aus den JSON-Daten extrahieren
+
+                    val allQuestions = it.getAllQuestions()
+                    Log.d("MainActivity", "Anzahl der Fragen: ${allQuestions.size}")
+                    // Die Questions in UserQuestions umwandeln
+                    val gameQuestions: List<Questions> = allQuestions.map { question ->
+                        question.toUserQuestion()
+                    }
+
+                    // Die Fragen in die Datenbank einfügen
+                    Log.d("MainActivity", "Vor Insert:")
+                    sharedViewModel.insertQuestions(gameQuestions)
+                    Log.d("MainActivity", "Nach Insert:")
+                }
+            } catch (e: Exception) {
+                // Fehlerbehandlung, z. B. Anzeige einer Fehlermeldung
+                Log.e("MainActivity", "Fehler Api -> Room: ${e.message}")
+            }
+        }
+
+        lifecycleScope.launch {
+            try {
+                val version = QuestionApi.retrofitService.getVersion()
+                Log.d("MainActivity", "Version: ${version.version}")
+            } catch (e: Exception) {
+                Log.e("MainActivity", "VersiongCheck Error: ${e.message}")
+            }
+        }
     }
 }
