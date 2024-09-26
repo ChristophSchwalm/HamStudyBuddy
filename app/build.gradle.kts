1ae283536a160b75520d7c5724ac326fc644e958
Christoph Schwalm <chris@christoph-schwalm.de>
2024-09-26 09:52:30 +0200

    HamStudyBuddy V0.080 Google V1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mlm4u.hamstudybuddy"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.2"
+        versionCode = 10
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
